7c358c85fb67f1e6b562f59384775d02d0233302
Chris <chrisyun92@gmail.com>
2018-10-09 10:33:03 -0700

    added crispr game

---
+++ CRISPR/Assets/Scripts/HyperlinkInText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+//Code from https://deltadreamgames.com/unity-tmp-hyperlinks
+public class HyperlinkInText : MonoBehaviour, IPointerClickHandler
+{
+    TextMeshProUGUI textMeshPro;
+    Canvas canvas;
+    Camera m_camera;
+
+    void Awake()
+    {
+        textMeshPro = gameObject.GetComponent<TextMeshProUGUI>();
+        canvas = gameObject.GetComponentInParent<Canvas>();
+        if (canvas.renderMode == RenderMode.ScreenSpaceOverlay)
+        {
+            m_camera = null;
+        }
+        else
+        {
+            m_camera = canvas.worldCamera;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        int linkIndex = TMP_TextUtilities.FindIntersectingLink(textMeshPro, Input.GetTouch(0).position, m_camera);
+        if (linkIndex != -1)
+        {
+            Debug.Log("inside");
+            TMP_LinkInfo linkInfo = textMeshPro.textInfo.linkInfo[linkIndex];
+            Debug.Log(linkInfo.GetLinkID());
+            Application.OpenURL(linkInfo.GetLinkID());
+        }
+    }
+}
